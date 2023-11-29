a86b5d976222c3d44c9036e2ae8702d274ce11f5
Pixoll <pixoll.gamer@gmail.com>
2023-11-28 23:45:18 -0300

    Copiar ejemplo de libGDX
    Ejemplo en https://libgdx.com/wiki/start/a-simple-game

--- desktop/src/udec/prog2/project/DesktopLauncher.java
+++ desktop/src/udec/prog2/project/DesktopLauncher.java
@@ -7,8 +7,10 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.useVsync(true);
         config.setForegroundFPS(120);
         config.setTitle("Zoo Simulator");
+        config.setMaximized(true);
         new Lwjgl3Application(new ZooSimulator(), config);
     }
 }
